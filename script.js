80fb062f5c94f00b2b78c1928a82fccd2229d139
K.e.v.y__ <tanakevy@gmail.com>
2026-06-14 14:31:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Welcome message
+console.log("Forbidden Fruit Loaded Successfully");
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
